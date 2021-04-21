47a65216db9c45c3c4a4f32e41f2f496efaa9dde
Airika Vettik <airikavettik@Airikas-iMac.lan>
2021-04-21 09:43:01 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3001
+const port = process.env.PORT || 3001
 const router = require('./src/router')
 
 
